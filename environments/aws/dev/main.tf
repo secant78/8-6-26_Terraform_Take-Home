8562b8ba3c1186c193e25db3bdcc64b4e48ad053
secant78 <scanny@udel.edu>
2026-08-09 21:53:03 -0400

    add changes to paths

--- environments/aws/dev/main.tf
+++ environments/aws/dev/main.tf
@@ -1,39 +1,21 @@
-terraform {
-  required_version = ">= 1.5.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 4.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-}
-
 # 1. VPC Module
 module "vpc" {
-  source      = "../../modules/vpc"
+  source      = "../../../modules/aws/vpc"
   vpc_cidr    = var.vpc_cidr
   environment = var.environment
 }
 
 # 2. Security Groups Module
 module "security_groups" {
-  source      = "../../modules/security_groups"
+  source      = "../../../modules/aws/security_group"
   vpc_id      = module.vpc.vpc_id
   vpc_cidr    = module.vpc.vpc_cidr
   environment = var.environment
 }
 
-# 3. EC2 Module (Private Subnet)
+# 3. EC2 Module
 module "ec2" {
-  source            = "../../modules/ec2"
+  source            = "../../../modules/aws/ec2"
   subnet_id         = module.vpc.private_subnet_a_id
   security_group_id = module.security_groups.ec2_security_group_id
   instance_type     = var.instance_type
@@ -42,7 +24,7 @@ module "ec2" {
 
 # 4. ALB Module
 module "alb" {
-  source                = "../../modules/alb"
+  source                = "../../../modules/aws/alb"
   vpc_id                = module.vpc.vpc_id
   public_subnet_ids     = module.vpc.public_subnet_ids
   alb_security_group_id = module.security_groups.alb_security_group_id
